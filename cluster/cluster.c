081430b93dc23131bb915bda77e7094d062c0feb
Vignesh Iyer <viiy1398@colorado.edu>
2019-11-30 19:56:35 -0700

    Few changes

--- cluster/cluster.c
+++ cluster/cluster.c
@@ -25,40 +25,49 @@ int main(int argc, char** argv)
 void create_cluster(int world_size,int world_rank)
 {
 
-  	// We are assuming at least 2 processes for this task
+  	int data_send;
+	int data_recv[10];
+	// We are assuming at least 2 processes for this task
   	if (world_size < 2) 
 	{
     		perror( "World size must be greater than 1 for " );
     		MPI_Abort(MPI_COMM_WORLD, 1);
   	}
-
-  	int number_1=7;
+	
+  	int number_1=0;
 	int number;
-  	if(world_rank==5)
-  	{
-		int check=MPI_Send(&number_1,1,MPI_INT,0,0,MPI_COMM_WORLD);
-  	}
-
-	 if (world_rank == 0) 
+	while(number_1<10)
 	{
-	 	int i=1;
-      		int check=MPI_Recv(&number_1, 1, MPI_INT,5,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-		if(check==0)
+  		if(world_rank==5)
+  		{
+			int check=MPI_Send(&number_1,1,MPI_INT,0,0,MPI_COMM_WORLD);
+  		}
+		else if (world_rank == 0) 
+		{	
+			int i=1;
+      			int check=MPI_Recv(&number_1, 1, MPI_INT,5,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+			if(check==0)
+			{
+    				printf("Data Received %d\n",number_1);
+				while(i>0 && i<world_size)
+    				{
+    					MPI_Send(&number_1,1,MPI_INT,i,0,MPI_COMM_WORLD);
+    					if(i==1)
+					{
+						data_recv[number_1]=number_1;
+					}
+					//data_recv[i][number_1]=number_1;
+					i++;
+    				}	
+ 			}
+		}	
+		else if(world_rank>0 && world_rank<5)
 		{
-    			printf("Data Received %d\n",number_1);
-			number=-1;
-			while(i>0 && i<world_size)
-    			{
-    				MPI_Send(&number,1,MPI_INT,i,0,MPI_COMM_WORLD);
-    				i++;
-    			}	
- 		}
-	}	
-	else 
-	{
-    		MPI_Recv(&number,1,MPI_INT,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-    		printf("Process %d received number %d from process 0\n",world_rank, number);
-  	}
+    			MPI_Recv(&number_1,1,MPI_INT,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+  		}
+		number_1++;
+	}
+
 }
 
 
